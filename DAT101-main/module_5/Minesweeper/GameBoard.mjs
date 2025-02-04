dd3dbad178adc8ea7e4247a3c9eaff85ba1cbbab
janizzh <janproagha@hotmail.com>
2025-02-04 15:51:17 +0100

    Removed shadow

---
+++ DAT101-main/module_5/Minesweeper/GameBoard.mjs
@@ -0,0 +1,29 @@
+"use strict";
+import libsSprite from "../../common/libs/libSprite_v2.mjs";
+import lib2d from "../../common/libs/lib2d_v2.mjs";
+
+export class TGameBoard{
+    #sptl = null; //TopLeft
+    #sptm = null; //TopMiddle
+    #sptr = null; //TopRight
+    #sprm = null; //RightMiddle
+    #sprb = null; //RightBottom
+    #spbm = null; //BottomMiddle
+    #spbl = null; //BottomLeft
+    #splm = null; //LeftMiddle
+
+    constructor(aSpcvs, aSpriteInfo,){
+        const pos = new lib2d.TPoint(0,0);
+        this.#sptl = new libsSprite.TSprite(aSpcvs, aSpriteInfo.TopLeft, pos);
+        pos.x = aSpriteInfo.TopLeft.width;
+        this.#sptm = new libsSprite.TSprite(aSpcvs, aSpriteInfo.TopMiddle, pos);
+        pos.x += aSpriteInfo.TopMiddle.width;
+        this.#sptr = new libsSprite.TSprite(aSpcvs, aSpriteInfo.TopRight, pos);
+    }
+
+    draw(){
+        this.#sptl.draw();
+        this.#sptm.draw();
+        this.#sptr.draw();
+    }
+} //End of TGameBoard Class
