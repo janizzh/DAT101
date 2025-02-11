0be4c1eeda075b776eb5f155443dd9f7dba71045
janizzh <janproagha@hotmail.com>
2025-02-11 15:45:18 +0100

    Updated Scoreboard - 2

--- DAT101-main/module_5/Minesweeper/ScoreBoard.mjs
+++ DAT101-main/module_5/Minesweeper/ScoreBoard.mjs
@@ -1,24 +1,34 @@
 "use strict";
 import libSprite from "../../common/libs/libSprite_v2.mjs";
 import lib2d from "../../common/libs/lib2d_v2.mjs";
-import { gameProps, SpriteInfoList } from "./Minesweeper.mjs";  
+import { gameLevel, gameProps, SpriteInfoList } from "./Minesweeper.mjs";  
 
 
 
 export class TScoreBoard{
-  #TSpriteNumberLeft; //For miner øvre venstre hjørne
-  #TSpriteNumberRight; //For tid øvre høyre hjørne
-  #TSpriteButton; //For smiley øverste midten
+  #TSpriteNumberLeftMine; //For miner øvre venstre hjørne
+  #TSpriteNumberRightTime; //For tid øvre høyre hjørne
+  #TSpriteButtonSmiley; //For smiley øverste midten
   constructor(aSpriteCanvas){
-    const pos = new lib2d.TPoint(80, 22)
-    this.#TSpriteNumberLeft = new libSprite.TSpriteNumber(aSpriteCanvas, SpriteInfoList.Numbers,  pos);
-    this.#TSpriteNumberRight = new libSprite.TSpriteNumber(aSpriteCanvas, SpriteInfoList.Numbers,  pos);
-    this.#TSpriteButton = new libSprite.TSpriteNumber(aSpriteCanvas, SpriteInfoList.ButtonSmiley,  pos);
+    const pos = new lib2d.TPoint(112, 22)
+    this.#TSpriteNumberLeftMine = new libSprite.TSpriteNumber(aSpriteCanvas, SpriteInfoList.Numbers,  pos);
+    this.#TSpriteNumberLeftMine.justify = libSprite.ESpriteNumberJustifyType.Right;
+    this.#TSpriteNumberLeftMine.digits = 3;
+    this.#TSpriteNumberLeftMine.value = gameLevel.Mines;
+
+    pos.x = aSpriteCanvas.canvas.width - 70;
+    this.#TSpriteNumberRightTime = new libSprite.TSpriteNumber(aSpriteCanvas, SpriteInfoList.Numbers,  pos);
+    this.#TSpriteNumberRightTime.justify = libSprite.ESpriteNumberJustifyType.Right;
+    this.#TSpriteNumberRightTime.digits = 3;
+    this.#TSpriteNumberRightTime.value = 0;
+
+
+    //this.#TSpriteButton = new libSprite.TSpriteNumber(aSpriteCanvas, SpriteInfoList.ButtonSmiley,  pos); 
   }
   draw(){
-    this.#TSpriteNumberLeft.draw();
-    this.#TSpriteNumberRight.draw();
-    this.#TSpriteButton.draw();
+    this.#TSpriteNumberLeftMine.draw();
+    this.#TSpriteNumberRightTime.draw();
+    //this.#TSpriteButtonSmiley.draw();
   }
   
 }
