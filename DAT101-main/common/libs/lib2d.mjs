e8f9131c872362ce052961872d14509067e64017
janizzh <janproagha@hotmail.com>
2025-02-25 12:22:07 +0100

    New game

--- DAT101-main/common/libs/lib2d.mjs
+++ DAT101-main/common/libs/lib2d.mjs
@@ -4,121 +4,117 @@
  * @description A library for classes that manage 2D graphics.
  */
 
-class TPoint{
-    x;
-    y;
-    constructor(aX, aY){
+class TPoint {
+  x = 0;
+  y = 0;
+  constructor(aX, aY) {
     this.x = aX;
     this.y = aY;
-    }
-}
+  }
+} // End of TPoint class
 
-class TPosition extends TPoint{ //extends command arver public variabler og funksjoner fra klassen Tpoint så slipper man å deklarere igjen
-    constructor(aX, aY){ 
-        super(aX, aY); // er fordi det skal være en super classe SuperClass
-    }
+class TPosition extends TPoint {
+  constructor(aX, aY) {
+    super(aX, aY);
+  }
 
-    clone(){
-        return new TPosition(this.x, this.y);
-    }
+  clone() {
+    return new TPosition(this.x, this.y);
+  }
 
-    distanceToPoint(aPoint){ // måle distansen mellom 2 punkt, bruker så pythagoras for å finne hypotenus
-        const dx = this.x - aPoint.x;
-        const dy = this.y - aPoint.y;
-        return Math.hypot(dx, dy);
-    }
-
+  distanceToPoint(aPoint) {
+    const dx = this.x - aPoint.x;
+    const dy = this.y - aPoint.y;
+    return Math.hypot(dx, dy);
+  }
 } // End of TPosition class
 
-class TRectangle extends TPosition{ //Trecangle arver fra Tposition ved å bruke extends command
-    constructor(aX, aY, aWidth, aHeight){
-        super(aX, aY);
-        this.width = aWidth;
-        this.height = aHeight;
-    }
+class TRectangle extends TPosition {
+  constructor(aX, aY, aWidth, aHeight) {
+    super(aX, aY);
+    this.width = aWidth;
+    this.height = aHeight;
+  }
 
-    get left(){
-        return this.x;
-    }
+  get left() {
+    return this.x;
+  }
 
-    get right(){
-        return this.x + this.width;
-    }
+  get right() {
+    return this.x + this.width;
+  }
 
-    get top(){
-        return this.y;
-    }
+  get top() {
+    return this.y;
+  }
 
-    get bottom(){
-        return this.y + this.height;
-    }
+  get bottom() {
+    return this.y + this.height;
+  }
 
-
-    isInsideRect(aRect){
-    if(this.left >= aRect.right) return false;
-    if(this.right <= aRect.left) return false;
-    if(this.top >= aRect.bottom) return false;
-    if(this.bottom <= aRect.top) return false;
+  isInsideRect(aRect) {
+    if (this.left >= aRect.right) return false;
+    if (this.right <= aRect.left) return false;
+    if (this.top >= aRect.bottom) return false;
+    if (this.bottom <= aRect.top) return false;
     return true;
-    }
+  }
 
-    isPositionInside(aPosition){
-        if(this.left >= aPosition.x) return false;
-        if(this.right <= aPosition.x) return false;
-        if(this.top >= aPosition.y) return false;
-        if(this.bottom <= aPosition.y) return false;
-        return true;
-    }
+  isPositionInside(aPosition) {
+    if (this.left >= aPosition.x) return false;
+    if (this.right <= aPosition.x) return false;
+    if (this.top >= aPosition.y) return false;
+    if (this.bottom <= aPosition.y) return false;
+    return true;
+  }
 
-    get center(){
-        return new TPosition(this.x + this.width / 2, this.y + this.height / 2);
-      }
-    
-      set center(aPoint){
-        this.x = aPoint.x - this.width / 2;
-        this.y = aPoint.y - this.height / 2;
-      }
-} // End of Trectangle class
+  get center() {
+    return new TPosition(this.x + this.width / 2, this.y + this.height / 2);
+  }
 
-const RAD = Math.PI / 180; // For å få radianer istedenfor grader
+  set center(aPoint) {
+    this.x = aPoint.x - this.width / 2;
+    this.y = aPoint.y - this.height / 2;
+  }
+} // End of TRectangle class
+
+const RAD = Math.PI / 180;
 
 class TSineWave {
-    #amplitude;
-    #frequency;
-    #angle;
-    constructor(aAmplitude, aFrequency){
-        this.#amplitude = aAmplitude;
-        this.#frequency = aFrequency;
-        this.#angle = 0;
-    }
+  #amplitude;
+  #frequency;
+  #angle;
+  constructor(aAmplitude, aFrequency) {
+    this.#amplitude = aAmplitude;
+    this.#frequency = aFrequency;
+    this.#angle = 0;
+  }
 
-    get value(){
-        let value = this.#amplitude * Math.sin(this.#angle * RAD);
-        this.#angle += this.#frequency;
-        return value;
-    }
-
-} // End of TSinWave class
-
+  get value() {
+    let value = this.#amplitude * Math.sin(this.#angle * RAD);
+    this.#angle += this.#frequency;
+    return value;
+  }
+} // end of TSineWave class
 
 export default {
-    /**
-     * @class TPoint
-     * @description A class representation for x and y point in 2D.
-     * @param {number} aX - The x-coordinate.
-     * @param {number} aY - The y-coordinate.
-     */
-    TPoint: TPoint,
-    /**
-     * @class TPosition
-     * @description A position class for manupulation of point in 2D.
-     * @param {number} aX - The x-coordinate.
-     * @param {number} aY - The y-coordinate.
-     * @extends TPoint
-     * @method clone - A method to clone the position object.
-     * @method distanceToPoint  - A method to calculate the distance to another point
-     */
-    TPosition,
+  /**
+   * @class TPoint
+   * @description A class representation for x and y point in 2D.
+   * @param {number} aX - The x-coordinate.
+   * @param {number} aY - The y-coordinate.
+   */
+  TPoint,
+  /**
+   * @class TPosition
+   * @description A position class for manipulation of point in 2D.
+   * @param {number} aX - The x-coordinate.
+   * @param {number} aY - The y-coordinate.
+   * @extends TPoint
+   * @method clone - A method to clone the position object.
+   * @method distanceToPoint - A method to calculate the distance to another point.
+   */
+  TPosition,
 
   /**
    * @class TRectangle
@@ -130,7 +126,7 @@ export default {
    * @param {number} aHeight - The height of the rectangle.
    * @property {number} width - The width of the rectangle.
    * @property {number} height - The height of the rectangle.
-   * @property {number} left - The left side of the rectangle. 
+   * @property {number} left - The left side of the rectangle.
    * @property {number} right - The right side of the rectangle.
    * @property {number} top - The top side of the rectangle.
    * @property {number} bottom - The bottom side of the rectangle.
@@ -143,5 +139,5 @@ export default {
    * @param {number} aFrequency - The frequency of the wave.
    * @property {number} value - The next value of the wave.
    */
-  TSineWave
-}
+  TSineWave,
+};
--- DAT101-main/common/libs/libSprite.mjs
+++ DAT101-main/common/libs/libSprite.mjs
@@ -1,5 +1,5 @@
 "use strict";
-import lib2d from "./lib2d.mjs";
+import lib2D from "./lib2d.mjs";
 /**
  * @library libSprite
  * @description A library for classes that manage sprite animations.
@@ -16,7 +16,7 @@ class TSpriteCanvas {
     this.#ctx = aCanvas.getContext("2d");
     this.#img = new Image();
     this.#boundingRect = this.#cvs.getBoundingClientRect();
-    this.mousePos = new lib2d.TPoint(0, 0);
+    this.mousePos = new lib2D.TPosition(0, 0);
   }
 
   loadSpriteSheet(aFileName, aLoadedFinal) {
@@ -26,7 +26,7 @@ class TSpriteCanvas {
 
   drawSprite(aSpriteInfo, aDx = 0, aDy = 0, aIndex = 0, aRot = 0) {
     let index = aIndex;
-    const sx = aSpriteInfo.x + (index * aSpriteInfo.width);
+    const sx = aSpriteInfo.x + index * aSpriteInfo.width;
     const sy = aSpriteInfo.y;
     const sw = aSpriteInfo.width;
     const sh = aSpriteInfo.height;
@@ -35,30 +35,28 @@ class TSpriteCanvas {
     const dw = sw;
     const dh = sh;
     if(aRot !== 0){
-      //Hvis vi har rotasjon må vi flytte midten av destinasjonen til 0,0
-      const cx = dx + dw / 2; // cx = centerx
-      const cy = dy + dh / 2; // cy = centery
+      //Hvis vi har rotasjon må vi flytte mitten av destinasjonen til 0,0
+      const cx = dx + dw / 2;
+      const cy = dy + dh / 2;
       const rad = aRot * Math.PI / 180;
-      this.#ctx.translate(cx,cy);
+      this.#ctx.translate(cx, cy);
       this.#ctx.rotate(rad);
-      this.#ctx.drawImage(this.#img, sx, sy, sw, sh, -dw / 2, -dh / 2, dw, dh)
+      this.#ctx.drawImage(this.#img, sx, sy, sw, sh, -dw / 2, -dh / 2, dw, dh);
       this.#ctx.rotate(-rad);
       this.#ctx.translate(-cx, -cy);
-     } else{
-        this.#ctx.drawImage(this.#img, sx, sy, sw, sh, dx, dy, dw, dh);
-
+    }else{
+      this.#ctx.drawImage(this.#img, sx, sy, sw, sh, dx, dy, dw, dh);
     }
-    
   } // End of drawSprite
 
   drawText(aText, aPos){
-    this.#ctx.font = "20px Arial";
-    this.#ctx.fillStyle = "lightblack";
+    this.#ctx.font = "25px Arial";
+    this.#ctx.fillStyle = "#333333";
     this.#ctx.textAlign = "right";
     this.#ctx.fillText(aText, aPos.x, aPos.y);
   }
 
-  clearCanvas(){
+  clearCanvas() {
     this.#ctx.clearRect(0, 0, this.#cvs.width, this.#cvs.height);
   }
 
@@ -78,104 +76,102 @@ class TSpriteCanvas {
 
 } // End of TSpriteCanvas class
 
-class TSprite{
-  #spcvs; //use hashtag # to make the variable private
+/* 
+ Utvid konstruktøren til å ta inn et punkt for destinasjon til sprite.
+*/
+
+class TSprite {
+  #spcvs;
   #spi;
   #pos;
   #index;
   #speedIndex;
-  constructor(aSpriteCanvas, aSpriteInfo, aPosition){
-    this.#spcvs= aSpriteCanvas
-    this.#spi = aSpriteInfo
-    this.#pos = aPosition.clone(); // Vi trenger en kopi av posisjonen
+  constructor(aSpriteCanvas, aSpriteInfo, aPosition) {
+    this.#spcvs = aSpriteCanvas;
+    this.#spi = aSpriteInfo;
+    this.#pos = aPosition.clone(); //Vi trenger en kopi av posisjonen
     this.#index = 0;
     this.animateSpeed = 0;
     this.#speedIndex = 0;
-    this.boundingBox = new lib2d.TRectangle(this.#pos.x, this.#pos.y, this.#spi.width, this.#spi.height)
+    this.boundingBox = new lib2D.TRectangle(this.#pos.x, this.#pos.y, this.#spi.width, this.#spi.height);
     this.rotation = 0;
   }
 
-  
-  draw(){
-    if(this.animateSpeed > 0){
+  draw() {
+    if (this.animateSpeed > 0) {
       this.#speedIndex += this.animateSpeed / 100;
-      if(this.#speedIndex >= 1){
+      if (this.#speedIndex >= 1) {
         this.#index++;
         this.#speedIndex = 0;
-        if (this.#index >= this.#spi.count){
+        if (this.#index >= this.#spi.count) {
           this.#index = 0;
         }
       }
     }
-    this.#spcvs.drawSprite(this.#spi, this.#pos.x, this.#pos.y, this.#index, this.rotation)
-
+    this.#spcvs.drawSprite(this.#spi, this.#pos.x, this.#pos.y, this.#index, this.rotation);
   }
 
+  translate(aDx, aDy) {
+    this.#pos.x += aDx;
+    this.#pos.y += aDy;
+    this.boundingBox.x += aDx;
+    this.boundingBox.y += aDy;
+  }
 
+  get posX() {
+    return this.#pos.x;
+  }
 
-translate(aDx, aDy){
-  this.#pos.x += aDx;
-  this.#pos.y += aDy;
-  this.boundingBox.x += aDx;
-  this.boundingBox.y += aDy;
-}
-
-get posX(){
-  return this.#pos.x;
-
-}  
-
-get left(){
-  return this.#pos.x;
-}
-
-get right(){
-  return this.#pos.x + this.#spi.width;
-}
-
-set posX(aX){
-  this.#pos.x = aX;
-  this.boundingBox.x = aX;
-}
-
-set posY(aY){
-  this.#pos.y = aY;
-  this.boundingBox.y = aY;
-}
-
-  get posY(){
+  get posY() {
     return this.#pos.y;
-}
+  }
 
-setPos(aX, aY){
-  this.#pos.x = aX;
-  this.#pos.y = aY;
-  this.boundingBox.x = aX;
-  this.boundingBox.y = aY;
-}
+  get left(){
+    return this.#pos.x;
+  }
 
-getPos(){
-  return this.#pos;
-}
+  get right(){
+    return this.#pos.x + this.#spi.width;
+  }
 
-get index(){
-  return this.#index;
-}
+  set posX(aX) {
+    this.#pos.x = aX;
+    this.boundingBox.x = aX;
+  }
 
-set index(aIndex){
-  this.#index = aIndex;
-}
+  set posY(aY) {
+    this.#pos.y = aY;
+    this.boundingBox.y = aY;
+  }
 
-hasCollided(aSprite){
-  return this.boundingBox.isInsideRect(aSprite.boundingBox);
-}
+  setPos(aX, aY) {
+    this.#pos.x = aX;
+    this.#pos.y = aY;
+    this.boundingBox.x = aX;
+    this.boundingBox.y = aY;
+  }
 
-getCenter(){
-  return this.boundingBox.center;
-}
+  getPos(){
+    return this.#pos;
+  }
 
-} // End of TSprite class
+  get index() {
+    return this.#index;
+  }
+  
+  set index(aIndex){
+    this.#index = aIndex;
+  }
 
+  hasCollided(aSprite){
+    return this.boundingBox.isInsideRect(aSprite.boundingBox);
+  }
+
+  getCenter(){
+    return this.boundingBox.center;
+  }
+
+} //End of TSprite class
 
 export default {
   /**
@@ -187,8 +183,8 @@ export default {
    * @param {function} aLoadedFinal - A callback function to call when the image is done loading.
    */
   TSpriteCanvas: TSpriteCanvas,
-  
-/**
+
+  /**
    * @class TSprite
    * @description A class that manage sprite animations.
    * @param {TSpriteCanvas} aSpriteCanvas - The sprite canvas to use.
@@ -196,6 +192,5 @@ export default {
    * @param {TPosition} aPosition - The position of the sprite.
    * @function draw - Draws the sprite on the canvas.
    */
-TSprite: TSprite
-
+  TSprite: TSprite,
 };
